de22703b62642439507c317b3beb286cc518a515
Alexander Briones <alexanderbvart@gmail.com>
2021-02-11 10:28:04 -0500

    url de inicio corregida

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -2,7 +2,7 @@ const Header = ()=>{
     const view = `
         <div class="Header-main">
             <div class="Header-logo">
-                <h1><a href="/">100tifi.co</a></h1>
+                <h1><a href="https://alexbvart.github.io/spa-vanilla-js/">100tifi.co</a></h1>
             </div>
             <div class="Header-nav">
                 <a href="#/about/">About</a>
